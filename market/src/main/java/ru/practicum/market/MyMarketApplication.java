53a2224b65aa4ab3c7439e011204e4050b6185f7
DLinnik <Danil.linn2015@yandex.ru>
2026-01-08 19:29:29 +0300

    Создан скелет второго модуля - payments
    Took 1 hour 11 minutes

--- market/src/main/java/ru/practicum/market/MyMarketApplication.java
+++ market/src/main/java/ru/practicum/market/MyMarketApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MyMarketAppApplication {
+public class MyMarketApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MyMarketAppApplication.class, args);
+        SpringApplication.run(MyMarketApplication.class, args);
     }
 
 }
